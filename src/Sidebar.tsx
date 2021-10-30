c5508ad616ea28f549c0b44c66d2394772f9c618
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2021-10-30 22:38:34 +0100

    changes

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import { FaDiscord } from 'react-icons/fa';
+import React, { ReactNode } from 'react';
+import { FaDiscord, FaCompass } from 'react-icons/fa';
+import { BsPlus, BsDownload } from 'react-icons/bs';
 
 interface Props {
   //   children: React.ReactNode;
 }
 
+function Divider() {
+  return <div className="w-9 h-px my-0.5 bg-iconGray" />;
+}
+
 export const Sidebar = (props: Props) => {
   return (
-    <div className="fixed bg-gray-500 top-0 left-0 text-iconGreen h-screen flex flex-col shadow-lg">
+    <div className="fixed bg-gray-500 top-0 left-0 text-iconGreen h-screen flex flex-col items-center shadow-lg">
       <SidebarIcon icon={<FaDiscord size="28" />} home={true} active={true} tooltip="Home" />
-      <SidebarIcon icon={<FaDiscord size="28" />} />
-      <SidebarIcon icon={<FaDiscord size="28" />} />
-      <SidebarIcon icon={<FaDiscord size="28" />} />
+      <SidebarIcon icon={<BsPlus size="38" />} tooltip="Add Server" />
+      <SidebarIcon icon={<FaCompass size="26" />} tooltip="Explore" />
+      <Divider />
+      <SidebarIcon icon={<BsDownload size="24" />} tooltip="Download" />
     </div>
   );
 };
@@ -27,33 +33,45 @@ const SidebarIcon = ({
   active?: boolean;
   tooltip?: string;
 }) => (
-  <div className="flex items-center">
-    <div
-      className={
-        'flex items-center group bg-iconGray my-2 mx-3 p-3 hover:rounded-2xl hover:bg-iconGreen hover:text-white rounded-[2rem] transition-all duration-150 ' +
-        (home ? '!bg-blue-600 !text-white rounded-2xl' : '')
-      }
-    >
-      <div
-        className={
-          (active ? 'py-[10px] px-[3px]' : 'py-[7px] px-[2px]') +
-          ' absolute left-0  bg-white rounded-r-3xl group-hover:py-3 group-hover:px-[3px] transition-all duration-100 ' +
-          (home ? '!py-4' : '')
-        }
-      ></div>
-      {icon}
-      <Tooltip>{tooltip}</Tooltip>
-    </div>
-  </div>
+  <SidebarIconContainer home={home}>
+    <IconAria active={active} home={home} />
+    {icon}
+    <Tooltip>{tooltip}</Tooltip>
+  </SidebarIconContainer>
 );
 
-function Tooltip({ children = 'tooltip' }: { children?: React.ReactNode }) {
+function SidebarIconContainer(props: { home?: boolean; children: ReactNode }) {
   return (
-    <div className="absolute left-[80px] flex items-center scale-0 group-hover:scale-100 transition-all duration-75">
+    <div
+      className={
+        'flex items-center my-2 mx-3 justify-center group bg-iconGray h-12 w-12 rounded-3xl hover:rounded-xl hover:bg-iconGreen hover:text-white transition-all ease-linear duration-300 ' +
+        (props.home ? '!bg-blue-600 !text-white rounded-2xl' : '')
+      }
+    >
+      {props.children}
+    </div>
+  );
+}
+
+function IconAria(props: { active?: boolean; home?: boolean }) {
+  return (
+    <div
+      className={
+        (props.active ? 'py-[10px] px-[3px]' : 'py-[7px] px-[2px]') +
+        ' absolute left-0  bg-white rounded-r-3xl group-hover:py-3 group-hover:px-[3px] transition-all duration-100 ' +
+        (props.home ? '!py-4' : '')
+      }
+    ></div>
+  );
+}
+
+function Tooltip({ children = 'tooltip' }: { children?: string }) {
+  return (
+    <div className="absolute left-[80px]  flex items-center scale-0 group-hover:scale-100 transition-all duration-75">
       <div className="absolute p-1 bg-black text-white rotate-45" />
-      <div className="absolute left-[4px] py-1 px-2 rounded-lg bg-black text-white ">
+      <p className="absolute left-[4px] py-1 px-2 rounded-lg bg-black text-white min-w-max ">
         {children}
-      </div>
+      </p>
     </div>
   );
 }
